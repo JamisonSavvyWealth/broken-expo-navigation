12c94ca85b16075bd14404228dc04a8d1c37ba98
JamisonSavvyWealth <Jamison@savvywealth.com>
2024-08-12 14:34:18 -0400

    Initialize Hello World

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Login() {
+  return (
+    <View style={{ height: 300 }}>
+      <Text>Hello, World</Text>
+    </View>
+  );
+}
